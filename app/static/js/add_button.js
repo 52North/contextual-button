a225e28b71a5202947b44749b19c167d14216f07
JanVan01 <j_vanz01@uni-muenster.de>
2017-08-26 17:45:02 +0200

    fix lint warnings

--- app/static/js/add_button.js
+++ app/static/js/add_button.js
@@ -1,30 +1,29 @@
-var form = (function() {
-
-  var setLatLon = function(lat, lon) {
-    $('#lat').val(lat);
-    $('#lon').val(lon);
-  };
-
-  var getLatLon = function() {
-    return [$('#lat').val(), $('#lon').val()];
+var form = (function () {
+  var setLatLon = function (lat, lon) {
+    $('#lat').val(lat)
+    $('#lon').val(lon)
   }
 
-  var submit = function() {
-    var data = {};
+  var getLatLon = function () {
+    return [$('#lat').val(), $('#lon').val()]
+  }
 
-    $('#button-form').serializeArray().forEach(function(ele){
-      data[ele.name] = ele.value;
-    });
+  var submit = function () {
+    var data = {}
+
+    $('#button-form').serializeArray().forEach(function (ele) {
+      data[ele.name] = ele.value
+    })
 
     $.ajax({
       type: 'POST',
       url: '/api/v1/sensors',
       data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
       success: displaySuccess,
       failure: displayError
-    });
+    })
   }
 
   var displaySuccess = function(data) {
@@ -38,8 +37,8 @@ var form = (function() {
     $('#error-message').text(data)
   }
 
-  var init = function() {
-    $('#button-form').submit(function(e) {
+  var init = function () {
+    $('#button-form').submit(function (e) {
       e.preventDefault()
       submit()
     })
@@ -49,17 +48,17 @@ var form = (function() {
     setLatLon: setLatLon,
     getLatLon: getLatLon,
     init: init
-  };
-})();
+  }
+})()
 
-$(document).ready(function() {
-  form.init();
+$(document).ready(function () {
+  form.init()
 
-  map.init(form.getLatLon(), 16);
+  map.init(form.getLatLon(), 16)
   map.addMarker(form.getLatLon())
 
-  map.onClick(function(e){
+  map.onClick(function (e) {
     map.addMarker(e.latlng, true)
     form.setLatLon(e.latlng.lat, e.latlng.lng)
   })
-});
+})
--- app/static/js/common.js
+++ app/static/js/common.js
@@ -1,35 +1,34 @@
-var map = (function() {
+var map = (function () {
+  var _map
 
-  var _map;
+  var _markers = L.layerGroup()
 
-  var _markers = L.layerGroup();
-
-  var init = function(latLon, zoom) {
-    _map = L.map('map').setView(latLon, zoom);
+  var init = function (latLon, zoom) {
+    _map = L.map('map').setView(latLon, zoom)
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(_map);
+    }).addTo(_map)
 
     _markers.addTo(_map)
-  };
+  }
 
   var onClick = function (callback) {
-    _map.on('click', callback);
+    _map.on('click', callback)
   }
 
   var showPopUp = function (popup) {
-    popup.openOn(_map);
+    popup.openOn(_map)
   }
 
-  var addMarker = function (latlon, clearLayers=false) {
-    if(clearLayers){
+  var addMarker = function (latlon, clearLayers = false) {
+    if (clearLayers) {
       _markers.clearLayers()
     }
-    L.marker(latlon).addTo(_markers);
+    L.marker(latlon).addTo(_markers)
   }
 
-  var add = function(layer){
+  var add = function (layer) {
     layer.addTo(_map)
   }
 
@@ -39,5 +38,5 @@ var map = (function() {
     showPopUp: showPopUp,
     addMarker: addMarker,
     add: add
-  };
-})();
+  }
+})()
