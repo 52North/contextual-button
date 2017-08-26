b342798af5dbd6c8005b960aa81bd90d18ff388a
JanVan01 <j_vanz01@uni-muenster.de>
2017-08-26 14:52:02 +0200

    move calls to sos to models module

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify, make_response
-from uuid import uuid4
-from datetime import datetime as dt, timezone
-import xmltodict
+from models import Sensor, Observation
 import requests
 
 app = Flask(__name__)
@@ -24,21 +22,7 @@ def add_button():
 @app.route("/api/v1/sensors", methods=['POST'])
 def create_sensor():
     sensor = request.get_json()
-    sensor['id'] = uuid4()
-    sensor['featureOfInterest'] = "http://example.org/featureOfInterest/" + \
-        str(uuid4())
-    sensor_xml = render_template('sensor.xml', sensor=sensor)
-    request_body = {"request": "InsertSensor",
-                    "service": "SOS",
-                    "version": "2.0.0",
-                    "procedureDescriptionFormat": "http://www.opengis.net/sensorml/2.0",
-                    "procedureDescription": sensor_xml,
-                    "observableProperty": ["http://example.org/button_press"],
-                    "observationType": ["http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation"],
-                    "featureOfInterestType": "http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint"
-                    }
-    sos_res = requests.post(
-        'http://sos:8080/52n-sos-webapp/service', json=request_body)
+    sos_res = Sensor().create(sensor)
     response = make_response(sos_res.text, sos_res.status_code)
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
@@ -46,29 +30,7 @@ def create_sensor():
 
 @app.route('/api/v1/sensors/<string:id>/observation', methods=['POST'])
 def create_observation(id):
-    uuid = str(uuid4())
-    time = dt.now(timezone.utc).isoformat(timespec='seconds')
-    # get the feature of interest that is connected to the sensor
-    featureOfInterest = getFeatureOfInterest(id)
-    request_body = {"request": "InsertObservation",
-                    "service": "SOS",
-                    "version": "2.0.0",
-                    "offering": "http://www.example.org/offering/{}/observations".format(id),
-                    "observation": {
-                        "identifier": {
-                            "value": uuid,
-                            "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"
-                        },
-                        "type": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation",
-                        "procedure": id,
-                        "observedProperty": "http://example.org/button_press",
-                        "featureOfInterest": featureOfInterest,
-                        "phenomenonTime": time,
-                        "resultTime": time,
-                        "result": 1
-                    }}
-    sos_res = requests.post(
-        'http://sos:8080/52n-sos-webapp/service', json=request_body)
+    sos_res = Observation().create(id)
     response = make_response(sos_res.text, sos_res.status_code)
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
@@ -82,61 +44,6 @@ def redirect_api_calls(path):
     response = make_response(sos_res.text, sos_res.status_code)
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
-
-
-def getFeatureOfInterest(id):
-    request_body = {
-        "request": "GetFeatureOfInterest",
-        "service": "SOS",
-        "version": "2.0.0",
-        "procedure": id
-    }
-    sos_res = requests.post(
-        'http://sos:8080/52n-sos-webapp/service', json=request_body)
-    # TODO check catch error
-    if 'featureOfInterest' in sos_res.json() and len(sos_res.json()['featureOfInterest']) > 0:
-        return sos_res.json()['featureOfInterest'][0]
-    else:
-        return createFeatureOfInterestFromSensor(id)
-
-
-def createFeatureOfInterestFromSensor(id):
-    request_body = {
-        "request": "DescribeSensor",
-        "service": "SOS",
-        "version": "2.0.0",
-        "procedureDescriptionFormat": "http://www.opengis.net/sensorml/2.0",
-        "procedure": id
-    }
-    sos_res = requests.post(
-        'http://sos:8080/52n-sos-webapp/service', json=request_body)
-    sensor_xml = sos_res.json()["procedureDescription"]["description"]
-    sensor = xmltodict.parse(sensor_xml)["sml:PhysicalComponent"]
-    featureOfInterest = {
-        "identifier": {
-            "value": sensor["sml:featuresOfInterest"]["sml:FeatureList"]["sml:feature"]["@xlink:href"],
-            "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"
-        },
-        "name": [
-            {"value": sensor["sml:identification"]["sml:IdentifierList"]["sml:identifier"][1]["sml:Term"]["sml:value"],
-            "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"}
-        ],
-        "geometry": {
-            "type": "Point",
-            "coordinates": [
-                float(sensor["sml:position"]["swe:Vector"]["swe:coordinate"][0]["swe:Quantity"]["swe:value"]),
-                float(sensor["sml:position"]["swe:Vector"]["swe:coordinate"][1]["swe:Quantity"]["swe:value"])
-            ],
-            "crs": {
-                "type": "name",
-                "properties": {
-                    "name": "EPSG:4326"
-                }
-            }
-        }
-    }
-    return featureOfInterest
-
 
 
 if __name__ == "__main__":
---
+++ app/models.py
@@ -0,0 +1,114 @@
+from flask import render_template
+from datetime import datetime as dt, timezone
+from uuid import uuid4
+import json
+import xmltodict
+import requests
+
+
+class Sensor:
+    def create(self, sensor):
+        sensor['id'] = uuid4()
+        sensor['featureOfInterest'] = "http://example.org/featureOfInterest/" + \
+            str(uuid4())
+        sensor_xml = render_template('sensor.xml', sensor=sensor)
+        request_body = {"request": "InsertSensor",
+                        "service": "SOS",
+                        "version": "2.0.0",
+                        "procedureDescriptionFormat": "http://www.opengis.net/sensorml/2.0",
+                        "procedureDescription": sensor_xml,
+                        "observableProperty": ["http://example.org/button_press"],
+                        "observationType": ["http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation"],
+                        "featureOfInterestType": "http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint"
+                        }
+        return requests.post(
+            'http://sos:8080/52n-sos-webapp/service', json=request_body)
+
+    def get(self, id):
+        request_body = {
+            "request": "DescribeSensor",
+            "service": "SOS",
+            "version": "2.0.0",
+            "procedureDescriptionFormat": "http://www.opengis.net/sensorml/2.0",
+            "procedure": id
+        }
+        sos_res = requests.post(
+            'http://sos:8080/52n-sos-webapp/service', json=request_body)
+        sensor_xml = sos_res.json()["procedureDescription"]["description"]
+        return xmltodict.parse(sensor_xml)
+
+
+class Observation:
+    def create(self, sensor_id):
+        uuid = str(uuid4())
+        time = dt.now(timezone.utc).isoformat(timespec='seconds')
+        # get the feature of interest that is connected to the sensor
+        feature_of_interest = self._get_feature_of_interest(sensor_id)
+        request_body = {"request": "InsertObservation",
+                        "service": "SOS",
+                        "version": "2.0.0",
+                        "offering": "http://www.example.org/offering/{}/observations".format(sensor_id),
+                        "observation": {
+                            "identifier": {
+                                "value": uuid,
+                                "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"
+                            },
+                            "type": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation",
+                            "procedure": sensor_id,
+                            "observedProperty": "http://example.org/button_press",
+                            "featureOfInterest": feature_of_interest,
+                            "phenomenonTime": time,
+                            "resultTime": time,
+                            "result": 1
+                        }}
+        return requests.post(
+            'http://sos:8080/52n-sos-webapp/service', json=request_body)
+
+    def _get_feature_of_interest(self, id):
+        # TODO try catch
+        foi_res = FeatureOfInterest().get(id).json()
+        if 'featureOfInterest' in foi_res and len(foi_res['featureOfInterest']) > 0:
+            return foi_res['featureOfInterest'][0]
+        else:
+            return self._create_feature_of_interest_from_sensor(id)
+
+    def _create_feature_of_interest_from_sensor(self, id):
+        sensor = Sensor().get(id)["sml:PhysicalComponent"]
+        feature_of_interest = {
+            "identifier": {
+                "value": sensor["sml:featuresOfInterest"]["sml:FeatureList"]["sml:feature"]["@xlink:href"],
+                "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"
+            },
+            "name": [
+                {"value": sensor["sml:identification"]["sml:IdentifierList"]["sml:identifier"][1]["sml:Term"]["sml:value"],
+                 "codespace": "http://www.opengis.net/def/nil/OGC/0/unknown"}
+            ],
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    float(sensor["sml:position"]["swe:Vector"]
+                          ["swe:coordinate"][0]["swe:Quantity"]["swe:value"]),
+                    float(sensor["sml:position"]["swe:Vector"]
+                          ["swe:coordinate"][1]["swe:Quantity"]["swe:value"])
+                ],
+                "crs": {
+                    "type": "name",
+                    "properties": {
+                        "name": "EPSG:4326"
+                    }
+                }
+            }
+        }
+        return feature_of_interest
+
+
+class FeatureOfInterest:
+    def get(self, sensor_id):
+        request_body = {
+            "request": "GetFeatureOfInterest",
+            "service": "SOS",
+            "version": "2.0.0",
+            "procedure": sensor_id
+        }
+        return requests.post(
+            'http://sos:8080/52n-sos-webapp/service', json=request_body)
